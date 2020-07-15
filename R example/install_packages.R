fb2b6e20fed4591ec3ee5318225225d2041fa0a5
Ian Ren <ianren.ontheway@gmail.com>
2020-07-15 10:39:06 -0700

    Update install_packages.R

--- R example/install_packages.R
+++ R example/install_packages.R
@@ -1,1 +1,4 @@
-install.packages("readr","dplyr","ggplot2","forcats")
+install.packages("readr",repos = "http://cran.us.r-project.org")
+install.packages("dplyr",repos = "http://cran.us.r-project.org")
+install.packages("ggplot2",repos = "http://cran.us.r-project.org")
+install.packages("forcats",repos = "http://cran.us.r-project.org")
