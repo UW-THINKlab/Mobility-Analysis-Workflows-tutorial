60fa4321a988fe57870b2a9dbff1cc4adcb8da7d
renyz <renyz@wingspan.cn>
2020-07-15 11:58:03 -0700

    update

---
+++ R_example/install_packages.R
@@ -0,0 +1,4 @@
+install.packages("readr",repos = "http://cran.us.r-project.org")
+install.packages("dplyr",repos = "http://cran.us.r-project.org")
+install.packages("ggplot2",repos = "http://cran.us.r-project.org")
+install.packages("forcats",repos = "http://cran.us.r-project.org")
